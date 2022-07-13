c79c49017f1201e320d260afa1e6934fc287a7b2
Nishit2011 <nranjan.2004@gmail.com>
2022-07-13 17:25:29 +0530

    prev next routing using paramMap

--- src/app/post-detail/post-detail.component.ts
+++ src/app/post-detail/post-detail.component.ts
@@ -1,20 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-post-detail',
-  template: `PostId----> {{postId}}`,
+  template: `<h2>
+  PostId----> {{postId}}
+  </h2>
+  <a (click)="goPrevious()">Previous</a>
+  <a (click)="goNext()">Next</a>
+  `,
   styleUrls: ['./post-detail.component.css']
 })
 export class PostDetailComponent implements OnInit {
   public postId: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    let id = (this.route.snapshot.paramMap.get('id'))
-    console.log(typeof this.route.snapshot.paramMap.get('id'))
-    this.postId = id
+    // let id = (this.route.snapshot.paramMap.get('id'))
+    // console.log(typeof this.route.snapshot.paramMap.get('id'))
+    // this.postId = id
+    this.route.paramMap.subscribe((params: ParamMap)=>{
+      let id = params.get('id')
+      this.postId = Number(id)
+    })
+  }
+
+  goPrevious(){
+    let prevId = parseInt(this.postId)-1
+    this.router.navigate(['/posts', prevId])
+  }
+  goNext(){
+    let nextId = parseInt(this.postId) +1
+    this.router.navigate(['/posts', nextId])
   }
 
 }
